472acc4c79ac7e54fddd531c18708c586346bf94
suphanida2545 <Suphanida1988@gmail.com>
2022-05-24 11:24:42 +0700

    อัปเดต

--- 1.hello.java
+++ 1.hello.java
@@ -1,6 +1,10 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World! 888888"); 
+        System.out.println("NAME     :Mr.Vit"); 
+        System.out.println("NICKNAME :vi");
+        System.out.println("DEPART   :ICT");
+        System.out.println("GRADE    :4"); 
+
     }
 }
 
